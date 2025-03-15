b2f2e4fe9ac68e32e2c915bd77adf00f4014d8f6
barisemre143 <135892347+barisemre143@users.noreply.github.com>
2025-03-15 15:21:47 +0200

    config.py yüklendi

---
+++ config.py
@@ -0,0 +1,8 @@
+DB_PATH = "news.db"
+DEBUG_MODE = True
+RSS_FEEDS = [
+    "https://yesilgazete.org/feed/",
+    "https://ekolojihaber.com/feed/",
+    "https://www.greenpeace.org/turkiye/rss/",
+    "https://www.wwf.org.tr/rss"
+]

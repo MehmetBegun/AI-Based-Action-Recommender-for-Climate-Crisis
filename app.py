c095a1fe2be750a1921f0bc2a12f2c348775b29a
Ahmet Berkay Avan <150008301+ahmetberkayyy@users.noreply.github.com>
2025-03-22 13:45:43 +0300

    manuel haber ekleme modülüne göre güncellendi

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ from modules.db_helper import init_db
 from modules.rss_scraper import fetch_rss
 from modules.lda_categorizer import categorize_news
 from modules.suggestions import action_suggestions
+from modules.manuel_entry import add_manual_news 
 import sqlite3
+from flask import request, redirect, url_for
+from modules.web_scraper import fetch_iklim_haber, fetch_wwf
 
 app = Flask(__name__, template_folder="templates")
 
@@ -28,6 +31,18 @@ def home():
             "suggestion": suggestion
         })
     return render_template("index.html", articles=articles)
+
+@app.route("/admin", methods=["GET", "POST"])
+def admin_panel():
+    if request.method == "POST":
+        title = request.form.get("title")
+        link = request.form.get("link")
+        summary = request.form.get("summary")
+        add_manual_news(title, link, summary)
+        return redirect(url_for("home"))
+    return render_template("admin.html")
+print("------------------------------------------")
+print(app.url_map)
 
 if __name__ == "__main__":
     init_db()
